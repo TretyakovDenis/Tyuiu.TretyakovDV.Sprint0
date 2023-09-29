8364e6dc0beb77b3a34ed2dfe92255ebe6752335
Tyuiu.TretyakovDV@outlook.com <trev1793@gmail.com>
2023-09-29 14:45:27 +0500

    task1

---
+++ Tyuiu.TretyakovDV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.TretyakovDV.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Денис";
+            int age = 22;
+            Console.WriteLine("Здравствуйте! Меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
