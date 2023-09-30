f7db23596bd78396152562430ad07e38dcffa9f8
Tyuiu.TretyakovDV@outlook.com <trev1793@gmail.com>
2023-09-30 16:08:33 +0500

    Task4

---
+++ Tyuiu.TretyakovDV.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TretyakovDV.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.TretyakovDV.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekedAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void ChekedSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Substraction(10, 5));
+        }
+        public void ChekedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void ChekedDivisionlValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
---
+++ Tyuiu.TretyakovDV.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TretyakovDV.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TretyakovDV.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ead0d9e-80a1-4b24-b209-199e91ccaa3b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
